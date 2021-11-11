d6fdb07f074e5e4af3b6817b771d4b182f1afb63
gianlucanesta <gianluca.nesta@gmail.com>
2021-11-11 16:32:15 +0100

    Exercise Completed

--- js/script.js
+++ js/script.js
@@ -3,3 +3,23 @@
 // poi chiedi il suo colore preferito
 // Infine scrivi sulla pagina nomecognomecolorepreferito21
 
+// Chiedo all'utente il suo nome
+const userName = prompt('Inserisci il tuo nome:');
+console.log(userName);
+
+// Chiedo all'utente il suo cognome
+const userLastName = prompt('Inserisci il tuo cognome:');
+console.log(userLastName);
+
+// Chiedo all'utente il suo colore preferito
+const userFavColor = prompt('Inserisci il tuo colore preferito:');
+console.log(userFavColor);
+
+// Concateno le stringhe
+const myInsecurePw ='Ciao la tua Password è: ' +  userName + userLastName + userFavColor + '21';
+// console.log(myInsecurePw);
+
+// Mando a schermo la password generata
+// alert(myInsecurePw);
+
+document.getElementById('password-generated').innerHTML = myInsecurePw;
